f86015559ba53d1854141f81847ab988d263bca1
Ruebin <rubnj9@gmail.com>
2026-03-04 10:06:02 +0530

    deployed to render

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -102,7 +107,8 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
